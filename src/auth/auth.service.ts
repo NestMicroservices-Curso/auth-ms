1df6b34b0e8c741f087ee793870b235d82f82e77
jhoanjimz2 <jhoan.developer@gmail.com>
2026-08-01 07:06:52 -0500

    feat: add NATS transport module for microservices communication
    feat: generate Prisma models and types for User entity
    
    feat: add generated query engine for Linux musl ARM64 with OpenSSL 3.0

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient }                     from '@prisma/client';
+
+@Injectable()
+export class AuthService extends PrismaClient implements OnModuleInit {
+
+    private readonly logger = new Logger('AuthService')
+
+    onModuleInit() {
+        this.$connect();
+        this.logger.log('Connected Mongo DB"S')
+    }
+
+}
---
+++ src/transports/nats.module.ts
@@ -0,0 +1,30 @@
+import { Module }                   from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, NATS_SERVICE }       from 'src/config';
+
+// AQUI SE CONECTA AL MICROSERVICIO A TRAVES DEL TRANSPORTADOR NATS
+@Module({
+    imports: [
+        ClientsModule.register([
+            {
+                name: NATS_SERVICE,
+                transport: Transport.NATS,
+                options: {
+                servers: envs.natsServers
+                }
+            },
+        ]),
+    ],
+    exports: [
+        ClientsModule.register([
+            {
+                name: NATS_SERVICE,
+                transport: Transport.NATS,
+                options: {
+                servers: envs.natsServers
+                }
+            },
+        ])
+    ]
+})
+export class NatsModule {}
